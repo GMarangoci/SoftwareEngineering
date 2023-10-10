9941d65d818263e816934e7e45e537aac2bde96a
Gabriel Marangoci <gabriel.mara900@gmail.com>
2023-10-10 18:10:51 +0100

    10/10 2nd Lab

--- Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
+++ Tasks/02 - Iteration/207-looping-N-times/207-looping-N-times.cpp
@@ -17,12 +17,41 @@ int main()
 
     // TASK A - count from 10 down to 0 in steps of -1
 
+    for (i = 10; i >= 0; i--)
+    {
+        printf("i=%d\n", i);
+
+    }
+
+    puts("---");
+
     // TASK B - count from 10 down to -10 in steps of -2
 
+    for (i = 10; i >= -10; i-=2)
+    {
+        printf("i=%d\n", i);
+
+    }
+
+    puts("---");
     // TASK C - count from 10 to 200 in steps of 10
+
+    for (i = 10; i <= 200; i += 10)
+    {
+        printf("i=%d\n", i);
+
+    }
+
+    puts("---");
 
     // TASK D - count from -10 down to 20 in steps of 5
 
+    for (i = -10; i <= 20; i += 5)
+    {
+        printf("i=%d\n", i);
+    }
+
+    puts("---");
     // TASK E - can you predict the largest value of i that is displayed?
     for (i = 0; i < 12; i += 3)
     {
